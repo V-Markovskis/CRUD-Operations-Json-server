cd4a1f805bfba32c89a95c754e17d83d065ab4ba
V-Markovskis <vlads.markovskis@gmail.com>
2023-11-20 22:03:10 +0200

    delete option added

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,6 +1,6 @@
 import axios, { all } from 'axios'; // library, HTTP requests execution
 
-const formContainer = document.querySelector<HTMLElement>('.js-movie-container');
+const formContainer = document.querySelector<HTMLDivElement>('.js-movie-container');
 
 type Movie = {
     id: number;
@@ -18,11 +18,33 @@ const allMovies = () => {
     result.then(({ data }) => {
         data.forEach((movie) => {
             formContainer.innerHTML += `
-            <p>${movie.nickname}</p>
-            <p>${movie.movie}</p>
-            <p>${movie.review}</p>
-            <p>${movie.evaluation}</p>
+            <div>
+                <p>${movie.nickname}</p>
+                <p>${movie.movie}</p>
+                <p>${movie.review}</p>
+                <p>${movie.evaluation}</p>
+                <button class="delete-button-js" data-movie-id=${movie.id}>Delete</button>
+            </div>
         `;
+        });
+        const movieDeleteButton = document.querySelectorAll<HTMLButtonElement>('.delete-button-js');
+        
+        movieDeleteButton.forEach((deleteButton) => {
+
+            deleteButton.addEventListener('click', () => {
+                //dataset DOM property, providing access to the data-attributes of the element
+                //data attributes in HTML must begin with the prefix "data-"
+                // const id = deleteButton.dataset.movieId;
+
+                //Destructurization
+                const { movieId } = deleteButton.dataset;
+                
+                console.log('deleteButton.dataset', deleteButton.dataset);
+
+                axios.delete(`http://localhost:3004/movies/${movieId}`).then(() => {
+                    allMovies();
+                });
+            });
         });
     });
 };
@@ -39,12 +61,19 @@ movieForm.addEventListener('submit', (event) => {
     
     console.log('Form values: ', formValues);
 
-    axios.post('http://localhost:3004/movies', {
+    axios.post<Movie>('http://localhost:3004/movies', {
         nickname: formValues[0],
         movie: formValues[1],
         review: formValues[2],
         evaluation: formValues[3],
     }).then(() => {
         allMovies();
+        //clear value of (input, textarea)
+        formElements.forEach((element) => {
+            element.value = '';
+        });
+    })
+    .catch((error) => {
+        console.error('Error posting data:', error);
     });
 });
