e4a3366548d522781e0025aaf07d9e663a9bcd6e
V-Markovskis <vlads.markovskis@gmail.com>
2023-11-20 18:57:44 +0200

    POST request in progress

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,8 +1,50 @@
-import $ from 'jquery';
-import sum from './utils/sum/sum';
+import axios, { all } from 'axios'; // library, HTTP requests execution
 
-console.log('Ready for coding');
+const formContainer = document.querySelector<HTMLElement>('.js-movie-container');
 
-console.log('Body jQuery node:', $('body'));
-console.log('Body javascript node:', document.querySelector('body'));
-console.log('2 + 3 =', sum(2, 3));
+type Movie = {
+    id: number;
+    nickname: string;
+    movie: string;
+    review: string;
+    evaluation: number;
+}
+
+const allMovies = () => {
+    const result = axios.get<Movie[]>('http://localhost:3004/movies');
+
+    formContainer.innerHTML = '';
+
+    result.then(({ data }) => {
+        data.forEach((movie) => {
+            formContainer.innerHTML += `
+            <p>${movie.nickname}</p>
+            <p>${movie.movie}</p>
+            <p>${movie.review}</p>
+            <p>${movie.evaluation}</p>
+        `;
+        });
+    });
+};
+
+allMovies();
+
+const movieForm = document.querySelector('.js-form-container');
+
+movieForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formElements = movieForm.querySelectorAll<HTMLInputElement | HTMLTextAreaElement>('input, textarea');
+    const formValues = Array.from(formElements).map((element) => element.value);
+    
+    console.log('Form values: ', formValues);
+
+    axios.post('http://localhost:3004/movies', {
+        nickname: formValues[0],
+        movie: formValues[1],
+        review: formValues[2],
+        evaluation: formValues[3],
+    }).then(() => {
+        allMovies();
+    });
+});
